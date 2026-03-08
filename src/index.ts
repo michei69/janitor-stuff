f7fc23ef76c142ccf65fd6f087bb43f6fa133546
Mikey <76266972+michei69@users.noreply.github.com>
2026-03-08 19:53:47 +0200

    remove commented stuff

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,8 @@ import patchSearch from "./search";
 import { bootstrap } from "./loader";
 import setupTTS, { ToggleDeltaTTS, ToggleTTS } from "./tts";
 import { disableEventLogger } from "./disableLogger";
-import { hookReactCreateElement } from "./ui/hooker";
 
 (async () => {
-    // function removeChakraSpan() {
-    //     document.querySelectorAll('span.__chakra_env').forEach(span => span.remove());
-    // }
-    // if (document.body) {
-    //     removeChakraSpan();
-    // } else {
-    //     document.addEventListener('DOMContentLoaded', removeChakraSpan);
-    // }
-
     // we js need a window object bruh
     const wnd: Window = typeof unsafeWindow != "undefined" ? unsafeWindow : window
     globalThis.wnd = wnd
